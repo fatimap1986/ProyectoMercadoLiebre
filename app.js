fc7d2016c744b4c4bb9a0ead583c24302f7e1502
fatimap1986 <fatimapugliese@yahoo.com.ar>
2022-10-08 22:02:05 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ("express")
+const app = express ()
+const path = require ("path")
+
+app.use (express.static ("public"))
+
+const port = process.env.PORT || 3001
+
+app.listen (port, () => console.log ("Servidor corriendo en el puerto ${port}"))
+
+app.get ("/", (req, res) => (res.sendFile (path.join (__dirname, "/views/home.html"))))
+
+app.get ("/register", (req, res) => (res.sendFile (path.join (__dirname, "/views/register.html"))))
+
